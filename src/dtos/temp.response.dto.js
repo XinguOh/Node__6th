2b698447dbd661f064aff6ca64311e3603bfda6d
XinguOh <ojyoung2490@gmail.com>
2024-05-28 16:23:32 +0900

    feat : 실습 과제 제출

---
+++ src/dtos/temp.response.dto.js
@@ -0,0 +1,11 @@
+export const tempResponseDTO = (data) => {
+    return {"testString" : data};
+}
+
+export const flagResponseDTO = (flag) => {
+    return {"flag" : flag};
+}
+
+export const missionResponseDTO = (data) => {
+    return {"mission" : data};
+}
